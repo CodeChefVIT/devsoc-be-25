cfd80e29a399b6453df085dab038410b4ed037c7
ABHINAVGARG05 <abhichhrp@gmail.com>
2024-12-31 16:34:17 +0530

    updated wrapper functions

--- pkg/utils/write.go
+++ pkg/utils/write.go
@@ -1,17 +1,3 @@
-// package utils
-
-// import (
-// 	"github.com/labstack/echo/v4"
-// )
-
-// func WriteError (r echo.Context,status int, err error) error {
-// 	return r.JSON(status, map[string]string{"error":err.Error()})
-// }
-
-// func WriteJSON(r echo.Context, status int, v any) error {
-// 	return r.JSON(status, map[string]interface{}{"message":v})
-// }
-
 package utils
 
 
