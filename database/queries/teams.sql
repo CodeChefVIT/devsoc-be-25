6de8c21c50f97d5ed0919204e2661293b20d410c
Soham Mahapatra <soham.mahapatra2005@gmail.com>
2025-01-01 20:55:25 +0530

    feat:added some admin team routes

--- database/queries/teams.sql
+++ database/queries/teams.sql
@@ -1,2 +1,8 @@
 -- name: GetTeamIDByCode :one
 SELECT id FROM teams WHERE code = $1;
+
+-- name: GetTeams :many
+SELECT * FROM teams;
+
+-- name: GetTeamById :one
+SELECT * FROM teams WHERE id = $1;
