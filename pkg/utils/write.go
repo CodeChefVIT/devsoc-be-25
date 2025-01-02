2572180d941a5276804469a5006283a0f92cec65
ABHINAVGARG05 <abhichhrp@gmail.com>
2025-01-02 13:28:22 +0530

    struct for passing errors and json

---
+++ pkg/utils/write.go
@@ -0,0 +1,35 @@
+package utils
+
+import(
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type StandardResponse struct {
+	Status int `json:"status"`
+	Message interface{} `json:"message,omitempty"`
+	Error string `json:"error,omitempty"`
+}
+
+func WriteJSON(c echo.Context, status int, message interface{}) error {
+	response := StandardResponse{
+		Status:  status,
+		Message: message,
+	}
+	log.Printf("Response: %+v", response)
+	return c.JSON(status, response)
+}
+
+func WriteError(c echo.Context, status int, err error) error {
+	if status < 400 {
+		status = http.StatusInternalServerError
+	}
+	response := StandardResponse{
+		Status: status,
+		Error:  err.Error(),
+	}
+	log.Printf("Error Response: %+v", response)
+	return c.JSON(status, response)
+}
