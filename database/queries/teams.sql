b21d243af023e293d533876c0a5d83c25506905a
ABHINAVGARG05 <abhichhrp@gmail.com>
2025-01-03 10:33:12 +0530

    Add models for users and team, Add SQL queries for team and user management

--- database/queries/teams.sql
+++ database/queries/teams.sql
@@ -1,2 +1,51 @@
 -- name: GetTeamIDByCode :one
 SELECT id FROM teams WHERE code = $1;
+
+-- name: FindTeam :one
+SELECT id,name,code,round_qualified FROM teams 
+WHERE code = $1 
+LIMIT 1;
+
+-- name: KickMemeber :exec
+UPDATE users
+SET team_id = NULL
+WHERE id = $1;
+
+-- name: LeaveTeam :exec
+UPDATE users 
+SET team_id = NULL
+WHERE id = $1;
+
+-- name: CreateTeam :one
+INSERT INTO teams (
+    id, name, number_of_people, round_qualified, code
+) VALUES (
+    $1, $2, $3, $4, $5
+)
+RETURNING *;
+
+-- name: DeleteTeam :exec
+DELETE FROM teams 
+WHERE id = $1;
+
+-- name: CountTeamMembers :one
+SELECT COUNT(*) FROM users
+WHERE team_id = $1;
+
+-- name: AddUserToTeam :exec
+UPDATE users
+SET team_id = $1
+WHERE id = $2;
+
+-- name: RemoveUserFromTeam :exec
+UPDATE users
+SET team_id = NULL
+WHERE team_id = $1 AND id = $2;
+
+-- name: GetUserByID :one
+SELECT * FROM users WHERE id = $1;
+
+-- name: UpdateUserTeam :exec
+UPDATE users
+SET team_id = $1, is_leader = $2
+WHERE id = $3;
