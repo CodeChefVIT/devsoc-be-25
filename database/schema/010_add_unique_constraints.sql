1d49952bc82b33f615792fbf5c9988f3ae10cc63
Vedant Matanhelia <vedant.matanhelia@gmail.com>
2025-01-30 02:06:49 +0530

    feat: add proper migrations

--- database/schema/010_add_unique_constraints.sql
+++ database/schema/010_add_unique_constraints.sql
@@ -1,6 +1,9 @@
 -- +goose Up
-ALTER TABLE users ADD UNIQUE (github_profile);
+ALTER TABLE users ADD CONSTRAINT unique_github_profile UNIQUE (github_profile);
 
-ALTER TABLE teams ADD UNIQUE (name);
+ALTER TABLE teams ADD CONSTRAINT unique_team_name UNIQUE (name);
 
 -- +goose Down
+ALTER TABLE users DROP CONSTRAINT unique_github_profile;
+
+ALTER TABLE teams DROP CONSTRAINT unique_team_name;
