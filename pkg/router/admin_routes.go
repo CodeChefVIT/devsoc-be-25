d22c681dcccf971d2760295f5cb8c2311274ba9a
Soham Mahapatra <soham.mahapatra2005@gmail.com>
2025-02-02 23:18:36 +0530

    fix

--- pkg/router/admin_routes.go
+++ pkg/router/admin_routes.go
@@ -2,13 +2,14 @@ package router
 
 import (
 	"github.com/CodeChefVIT/devsoc-be-24/pkg/controller"
+	"github.com/CodeChefVIT/devsoc-be-24/pkg/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func AdminRoutes(incomingRoutes *echo.Group) {
 	admin := incomingRoutes.Group("/admin")
-	// admin.Use(middleware.JWTMiddleware())
-	// admin.Use(middleware.CheckAdmin)
+	admin.Use(middleware.JWTMiddleware())
+	admin.Use(middleware.CheckAdmin)
 
 	admin.GET("/users", controller.GetAllUsers)
 	admin.GET("/user/:email", controller.GetUsersByEmail)
