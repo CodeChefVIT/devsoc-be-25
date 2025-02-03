0ccd0302e77f88ffc9ade3b3d0b70905bf8f9666
Soham Mahapatra <soham.mahapatra2005@gmail.com>
2025-02-03 15:00:28 +0530

    chipi chipi chapa chapa bogey bogey baga baga

--- pkg/models/panel.go
+++ pkg/models/panel.go
@@ -12,23 +12,23 @@ type CreatePanel struct {
 }
 
 type UpdateScore struct {
-	Design         int    `json:"design" validate:"required,min=0,max=10"`
-	Implementation int    `json:"implementation" validate:"required,min=0,max=10"`
-	Presentation   int    `json:"presentation" validate:"required,min=0,max=10"`
+	Design         int    `json:"design" validate:"required,min=-1,max=10"`
+	Implementation int    `json:"implementation" validate:"required,min=-1,max=10"`
+	Presentation   int    `json:"presentation" validate:"required,min=-1,max=10"`
 	Round          int    `json:"round" validate:"required"`
-	Innovation     int    `json:"innovation" validate:"required,min=0,max=10"`
-	Teamwork       int    `json:"teamwork" validate:"required,min=0,max=10"`
+	Innovation     int    `json:"innovation" validate:"required,min=-1,max=10"`
+	Teamwork       int    `json:"teamwork" validate:"required,min=-1,max=10"`
 	Comment        string `json:"comment"`
 	TeamID         string `json:"team_id" validate:"required,uuid"`
 }
 
 type CreateScore struct {
-	Design         int    `json:"design" validate:"required,min=0,max=10"`
-	Implementation int    `json:"implementation" validate:"required,min=0,max=10"`
-	Presentation   int    `json:"presentation" validate:"required,min=0,max=10"`
+	Design         int    `json:"design" validate:"required,min=-1,max=10"`
+	Implementation int    `json:"implementation" validate:"required,min=-1,max=10"`
+	Presentation   int    `json:"presentation" validate:"required,min=-1,max=10"`
 	Round          int    `json:"round" validate:"required"`
-	Innovation     int    `json:"innovation" validate:"required,min=0,max=10"`
-	Teamwork       int    `json:"teamwork" validate:"required,min=0,max=10"`
+	Innovation     int    `json:"innovation" validate:"required,min=-1,max=10"`
+	Teamwork       int    `json:"teamwork" validate:"required,min=-1,max=10"`
 	Comment        string `json:"comment"`
 	TeamID         string `json:"team_id" validate:"required,uuid"`
 }
